8646d41e48a62a329c3aafb358f05a90098078c0
Mariam <mariamz.dev@gmail.com>
2021-12-27 11:45:09 +0100

    higher-order-functions

---
+++ higher-order-functions.js
@@ -0,0 +1,10 @@
+function repeat(operation, num) {
+    function operation(){
+        console.log("cc");
+    }
+    num = 2;
+    for (let i = 0; i <= num; i++){
+        operation();
+    }
+  }
+  module.exports = repeat
